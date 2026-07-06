b3a7aa091185b1ef3db7ff54f95048dc2a0cbf73
ngominhquyen1309-sys <ngominhquyen1309@gmail.com>
2026-07-06 22:19:02 +0900

    NOP BAI

---
+++ baitap1-ss16/ss16-baitap1.js
@@ -0,0 +1,13 @@
+
+function processConfirm(answer) {
+
+    let result = "";
+
+    if (answer) {
+        result += "Excellent. We'll play a nice game of chess.";
+    } else {
+        result += "Maybe later then.";
+    }
+
+    return result;
+}
